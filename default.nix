603406a044e7e758d7ab8dafd8e49e5e944857b6
Ben Gamari <ben@smart-cactus.org>
2020-06-07 20:40:13 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, attoparsec, base, containers, data-default
+      , errors, lens, mtl, pandoc, pandoc-types, process, stdenv
+      , system-filepath, text, transformers, xml-conduit, xml-lens
+      }:
+      mkDerivation {
+        pname = "svg-filter";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          attoparsec base containers data-default errors lens mtl pandoc
+          pandoc-types process system-filepath text transformers xml-conduit
+          xml-lens
+        ];
+        license = "unknown";
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
